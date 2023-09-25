82ad196db2eac3712497c9aad3e5978730a1c258
AaronVelasco93 <aaronvelasco1933@outlook.com>
2023-09-25 10:36:35 -0600

    Cambio realizado por: Bered

--- Programa1/main.cpp
+++ Programa1/main.cpp
@@ -3,6 +3,11 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
+	int horas;
+	int mins;
+	int minres; 
+	printf("Mins:");
+	scanf("%i",mins);
 	
 	return 0;
 }
